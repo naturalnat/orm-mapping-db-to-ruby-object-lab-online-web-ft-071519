c00444d906a946c0ad7aecf664b474ce010491d4
Natalie Taktachev <ntaktach@gmail.com>
2019-08-20 00:09:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,9 +61,9 @@ class Student
 
   def students_below_12th_grade
     sql = <<-SQL
-      SELECT * 
+      SELECT *
       FROM students
-      WHERE grade < 12 
+      WHERE grade < 12
     SQL
 
     DB[:conn].execute(sql).map do |row|
